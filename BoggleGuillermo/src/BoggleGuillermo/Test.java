74ca1a0d576080b6333417209fb045b1a571f543
WillTama <72381691+WillTama@users.noreply.github.com>
2021-03-23 05:36:32 +0100

    Add files via upload

---
+++ BoggleGuillermo/src/BoggleGuillermo/Test.java
@@ -0,0 +1,18 @@
+package BoggleGuillermo;
+
+
+import java.util.Scanner;
+public class Test {
+  public static void main(String[] args) {
+    
+    Tablero ejemplo = new Tablero();
+   
+    System.out.println(ejemplo);
+    System.out.println(ejemplo.estaPalabraEnTablero("AJO"));
+    
+    
+  
+  
+  
+  }
+  }
